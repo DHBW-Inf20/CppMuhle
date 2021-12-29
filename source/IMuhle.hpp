ec898aa0f199091d8d8b4631cb4f47ee04785393
raphaelsack <i20029@hb.dhbw-stuttgart.de>
2021-12-29 10:01:35 +0100

    implemented Methods: placePiece, checkIfValid, movePiece, jumpPiece

--- source/IMuhle.hpp
+++ source/IMuhle.hpp
@@ -10,13 +10,15 @@ private:
     IView* view;
     int24 black;
     int24 white;
-
+    bool isWhiteTurn;
+    bool attackMode;
 public:
 virtual void processInput(std::string command) = 0;
 virtual void placePiece(std::string notation) = 0;
 virtual void movePiece(std::string notation) = 0;
 virtual void jumpPiece(std::string notation) = 0;
-virtual bool checkIfLegal(std::string notation) = 0;
+virtual bool checkIfLegalMove(std::string notation) = 0;
+virtual bool checkIfValid(std::string notation) = 0;
 virtual bool checkIf3(std::string lastMovedPiece) = 0;
 virtual void attack(std::string notation) = 0;
 
--- source/MuhleLogik.hpp
+++ source/MuhleLogik.hpp
@@ -8,15 +8,19 @@ class MuhleLogik : public IMuhle
 {
 private:
     IView* view;
-    
+    bool isWhiteTurn;
+    bool attackMode;
     int24 black;
     int24 white;
+    bool isOccupied(std::string position, int24& player);
+    int24 positionToBit24(std::string position);
 public:
     virtual void processInput(std::string command);
     virtual void placePiece(std::string notation);
     virtual void movePiece(std::string notation);
     virtual void jumpPiece(std::string notation);
-    virtual bool checkIfLegal(std::string notation);
+    virtual bool checkIfLegalMove(std::string notation);
+    virtual bool checkIfValid(std::string notation);
     virtual bool checkIf3(std::string lastMovedPiece);
     virtual void attack(std::string notation);
     virtual void initialize();
